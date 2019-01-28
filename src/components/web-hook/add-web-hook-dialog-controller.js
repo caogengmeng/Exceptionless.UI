5c044f16394c135dbb0cfc0a2726fcfe309e7764
耕萌 曹asxxx <395120787@qq.com>
2019-01-28 16:06:22 +0800

    增加前端界面WEBHOOK选项

--- src/components/web-hook/add-web-hook-dialog-controller.js
+++ src/components/web-hook/add-web-hook-dialog-controller.js
@@ -1,8 +1,36 @@
+
+Skip to content
+
+    Pull requests
+    Issues
+    Marketplace
+    Explore
+
+    @caogengmeng
+
+0
+0
+
+    55
+
+justmine66/Exceptionless.UI forked from exceptionless/Exceptionless.UI
+Code
+Pull requests 0
+Projects 0
+Wiki
+Insights
+Exceptionless.UI/src/components/web-hook/add-web-hook-dialog-controller.js
+0b6af4d 21 days ago
+据说甜蜜呢 Correspondence with backend，add new web hook types，LogWarn。
+@niemyjski
+@ejsmith
+@edwardmeng
+88 lines (80 sloc) 2.66 KB
 (function () {
   'use strict';
 
   angular.module('exceptionless.web-hook')
-    .controller('AddWebHookDialog', function ($ExceptionlessClient, $uibModalInstance, translateService) {
+    .controller('AddWebHookDialog', function ($ExceptionlessClient, $uibModalInstance) {
       var vm = this;
       function cancel() {
         $ExceptionlessClient.submitFeatureUsage(vm._source + '.cancel');
@@ -13,33 +41,48 @@
         return [
           {
             key: 'NewError',
-            name: translateService.T('New Error'),
-            description: translateService.T('Occurs when a new error that has never been seen before is reported to your project.')
+            name: 'New Error',
+            description: 'Occurs when a new error that has never been seen before is reported to your project.'
+          },
+		  {
+            key: 'Error',
+            name: 'Error',
+            description: 'Occurs when a error.'
+          },
+		  {
+            key: 'LogWarn',
+            name: 'LogWarn',
+            description: 'Occurs when a log warn.'
+          },
+		  {
+            key: 'LogError',
+            name: 'LogError',
+            description: 'Occurs when a log error.'
           },
           {
             key: 'CriticalError',
-            name: translateService.T('Critical Error'),
-            description: translateService.T('Occurs when an error that has been marked as critical is reported to your project.')
+            name: 'Critical Error',
+            description: 'Occurs when an error that has been marked as critical is reported to your project.'
           },
           {
             key: 'StackRegression',
-            name: translateService.T('Regression'),
-            description: translateService.T('Occurs when an event that has been marked as fixed has reoccurred in your project.')
+            name: 'Regression',
+            description: 'Occurs when an event that has been marked as fixed has reoccurred in your project.'
           },
           {
             key: 'NewEvent',
-            name: translateService.T('New Event'),
-            description: translateService.T('Occurs when a new event that has never been seen before is reported to your project.')
+            name: 'New Event',
+            description: 'Occurs when a new event that has never been seen before is reported to your project.'
           },
           {
             key: 'CriticalEvent',
-            name: translateService.T('Critical Event'),
-            description: translateService.T('Occurs when an event that has been marked as critical is reported to your project.')
+            name: 'Critical Event',
+            description: 'Occurs when an event that has been marked as critical is reported to your project.'
           },
           {
             key: 'StackPromoted',
-            name: translateService.T('Promoted'),
-            description: translateService.T('Used to promote event stacks to external systems.')
+            name: 'Promoted',
+            description: 'Used to promote event stacks to external systems.'
           }
         ];
       }
@@ -70,3 +113,19 @@
       };
     });
 }());
+
+    © 2019 GitHub, Inc.
+    Terms
+    Privacy
+    Security
+    Status
+    Help
+
+    Contact GitHub
+    Pricing
+    API
+    Training
+    Blog
+    About
+
+Press h to open a hovercard with more details.
